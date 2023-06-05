4969b5c00b0eed9f2e0e2bfdbb31a3262cfc510f
Victoriia-Ripka <vikaripka@gmail.com>
2023-06-05 11:30:47 +0200

    DB was connected to backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = require('./app');
 
-app.listen(8080, () => {
-  console.log('Server is running');
-});
+const DB_HOST = process.env.DB_HOST;
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log('connected to DB');
+    app.listen(PORT, () => {
+      console.log('Server is running on ', PORT);
+    });
+  })
+  .catch(err => {
+    console.log(err.message);
+    process.exit(1);
+  });
